db9ea1966bc3e41ae3e93a81c59d7ba5b429ace8
fatima <fatimafarhat425@gmail.com>
2026-07-06 10:32:37 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter-app';
+  count=0;
+ /*handleIncrement(){
+    this.count=this.count+1;
+  }
+ handleDecrement(){
+    this.count=this.count-1;
+  }
+  handleReset(){
+   this.count=0;
+  }*/
+  handleCounter(val:string){
+    if(val=='minus'){
+      if(this.count>0){
+        this.count=this.count-1;
+      }else{
+        this.count=0;
+      }
+    }else if(val=='plus'){
+      this.count=this.count+1;
+    }else{
+      this.count=0;
+    }
+  }
 }
